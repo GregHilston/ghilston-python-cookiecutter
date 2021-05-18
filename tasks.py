a467071004926b091f670905ba4c201a62257e9c
GregHilston <Gregory.Hilston@gmail.com>
2021-05-18 12:25:55 -0400

    Adds Invoke command to determine test coverage

--- tasks.py
+++ tasks.py
@@ -1,7 +1,23 @@
+import os
+
 from invoke import task
+
+
+ROOT_PATH = os.path.dirname(__file__)
+SOURCE_DIRECTORY = "app"
+SOURCE_PATH = os.path.join(ROOT_PATH, SOURCE_DIRECTORY)
+TEST_DIRECTORY = "test"
+TEST_PATH = os.path.join(ROOT_PATH, TEST_DIRECTORY)
+LOGS_DIRECTORY = "log"
+LOGS_PATH = os.path.join(ROOT_PATH, LOGS_DIRECTORY)
 
 
 @task
 def test(ctx):
-    """Runs Pytest test suite."""
+    """Runs Pytest test suite"""
     ctx.run("poetry run pytest", echo=True)
+
+@task
+def coverage(ctx):
+    """Produces test coverage"""
+    ctx.run("poetry run pytest --cov=app test/", echo=True)
