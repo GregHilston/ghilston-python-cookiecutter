773a0cf496ab7edf9713f4429b3b51c8184332c5
GregHilston <Gregory.Hilston@gmail.com>
2021-05-19 10:40:35 -0400

    Renames test dir and further adds pyproject configurations

--- tasks.py
+++ tasks.py
@@ -6,7 +6,7 @@ from invoke import task
 ROOT_PATH = os.path.dirname(__file__)
 SOURCE_DIRECTORY = "app"
 SOURCE_PATH = os.path.join(ROOT_PATH, SOURCE_DIRECTORY)
-TEST_DIRECTORY = "test"
+TEST_DIRECTORY = "tests"
 TEST_PATH = os.path.join(ROOT_PATH, TEST_DIRECTORY)
 LOGS_DIRECTORY = "log"
 LOGS_PATH = os.path.join(ROOT_PATH, LOGS_DIRECTORY)
