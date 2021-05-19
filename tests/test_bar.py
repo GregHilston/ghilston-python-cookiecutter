941dfd96acf0c836e20ec1364eee9a957a7c3e9e
GregHilston <Gregory.Hilston@gmail.com>
2021-05-19 14:47:45 -0400

    Adds documentation wording

--- tests/test_bar.py
+++ tests/test_bar.py
@@ -4,5 +4,5 @@ from ghilston_python_cookiecutter.bar.bar import example
 
 
 def test_bar():
-    """Demonstrates how to test our source code"""
+    """Demonstrates how to test our package source code"""
     assert example() == "bar"
