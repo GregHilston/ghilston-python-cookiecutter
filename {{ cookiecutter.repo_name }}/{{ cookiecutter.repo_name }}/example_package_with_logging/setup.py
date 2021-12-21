d3c7e289cc7abd7478f3faa1307048ac6f329295
GregHilston <Gregory.Hilston@gmail.com>
2021-12-20 21:28:35 -0500

    feat: adds example package using another package we wrote

--- {{ cookiecutter.repo_name }}/{{ cookiecutter.repo_name }}/example_package_with_logging/setup.py
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.repo_name }}/example_package_with_logging/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-from example_package import __version__
+from example_package_with_logging import __version__
 
 version = __version__
 
@@ -9,7 +9,7 @@ setup(
     description="Example Python package with logging",
     version=version,
     author_email="Gregory.Hilstoh@gmail.com",
-    url="https://github.com/GregHilston/ghilston-python-cookiecutter/tree/master/%7B%7B%20cookiecutter.repo_name%20%7D%7D/example_package",
+    url="https://github.com/GregHilston/ghilston-python-cookiecutter/tree/master/%7B%7B%20cookiecutter.repo_name%20%7D%7D/example_package_with_logging",
     entry_points={
         # Defines functions that a developer may access. Read more here https://stackoverflow.com/a/782984/1983957
         "console_scripts": [
